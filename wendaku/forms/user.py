67f4d30771d7ce31e343455f9e37487a819eee79
zhaoxinpeng <15531506906@sina.cn>
2018-04-26 20:04:17 +0800

    1.用户管理，增删改查

---
+++ wendaku/forms/user.py
@@ -0,0 +1,78 @@
+from django import forms
+
+from wendaku import models
+from publickFunc import account
+import datetime
+
+# 添加用户信息
+class UserForm(forms.Form):
+    username = forms.CharField(
+        required=True,
+        error_messages={
+            'required': "用户名不能为空"
+        }
+    )
+
+    password = forms.CharField(
+        required=True,
+        error_messages={
+            'required': "密码不能为空"
+        }
+    )
+    role_id = forms.IntegerField(
+        required=True,
+        error_messages={
+            'required': '角色不能为空'
+        }
+    )
+    # 查询用户名判断是否存在
+    def clean_username(self):
+        username = self.data['username']
+        # print(username)
+        objs = models.UserProfile.objects.filter(
+            username=username,
+        )
+        if objs:
+            self.add_error('username', '用户名已存在')
+        else:
+            return username
+
+    # 返回加密后的密码
+    def clean_password(self):
+        return account.str_encrypt(self.data['password'])
+
+
+# 更新用户信息
+class UserUpdateForm(forms.Form):
+    user_id = forms.IntegerField(
+        required=True,
+        error_messages={
+            'required': "用户id不能为空"
+        }
+    )
+
+    username = forms.CharField(
+        required=True,
+        error_messages={
+            'required': "用户名不能为空"
+        }
+    )
+
+    role_id = forms.IntegerField(
+        required=True,
+        error_messages={
+            'required': '角色不能为空'
+        }
+    )
+    # 判断用户名是否存在
+    def clean_username(self):
+        username = self.data['username']
+        user_id = self.data['user_id']
+        print(username)
+        objs = models.UserProfile.objects.filter(
+            username=username,
+        ).exclude(id=user_id)
+        if objs:
+            self.add_error('username', '用户名已存在')
+        else:
+            return username
---
+++ wendaku/views_dir/user.py
@@ -0,0 +1,120 @@
+from django.shortcuts import render
+from wendaku import models
+from publickFunc import Response
+from publickFunc import account
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+import time
+import datetime
+
+from wendaku.forms.user import UserForm, UserUpdateForm
+import json
+
+
+# cerf  token验证 用户展示模块
+@csrf_exempt
+@account.is_token(models.UserProfile)
+def user(request):
+    response = Response.ResponseObj()
+    if request.method == "GET":
+        # 链表查询   查询所有
+        userprofile_objs = models.UserProfile.objects.select_related('role', 'oper_user').all()
+        user_data = []
+        for obj in userprofile_objs:
+            #  如果有oper_user字段 等于本身名字
+            if obj.oper_user:
+                oper_user_username = obj.oper_user.username
+            else:
+                oper_user_username = ''
+
+            # print('oper_user_username -->', oper_user_username)
+            #  将查询出来的数据 加入列表
+            user_data.append({
+                'user_id': obj.id,
+                'username': obj.username,
+                'role': obj.role.name,
+                'create_date': obj.create_date,
+                'last_login_date': obj.last_login_date,
+                'oper_user': oper_user_username,
+            })
+            #  查询成功 返回200 状态码
+            response.code = 200
+            response.msg = '查询成功'
+            response.data = {
+                'user_data': user_data
+            }
+        else:
+            response.code = 402
+            response.msg = "请求异常"
+        return JsonResponse(response.__dict__)
+
+
+#  增删改 用户表
+#  csrf  token验证
+@csrf_exempt
+@account.is_token(models.UserProfile)
+def user_oper(request, oper_type, o_id):
+    response = Response.ResponseObj()
+    if request.method == "POST":
+        if oper_type == "add":
+            #  创建 form验证 实例（参数默认转成字典）
+            forms_obj = UserForm(request.POST)
+            if forms_obj.is_valid():
+                print("验证通过")
+                # print(forms_obj.cleaned_data)
+                #  添加数据库
+                # print('forms_obj.cleaned_data-->',forms_obj.cleaned_data)
+                models.UserProfile.objects.create(**forms_obj.cleaned_data)
+                response.code = 200
+                response.msg = "添加成功"
+            else:
+                print("验证不通过")
+                # print(forms_obj.errors)
+                response.code = 301
+                # print(forms_obj.errors.as_json())
+                response.msg = json.loads(forms_obj.errors.as_json())
+
+        elif oper_type == "delete":
+            # 删除 ID
+            models.UserProfile.objects.filter(id=o_id).delete()
+            response.code = 200
+            response.msg = "删除成功"
+
+        elif oper_type == "update":
+            # 获取ID 用户名 及 角色
+            form_data = {
+                'user_id': o_id,
+                'username': request.POST.get('username'),
+                'role_id': request.POST.get('role_id'),
+            }
+
+            forms_obj = UserUpdateForm(form_data)
+            if forms_obj.is_valid():
+                print("验证通过")
+                print(forms_obj.cleaned_data)
+                user_id = forms_obj.cleaned_data['user_id']
+                username = forms_obj.cleaned_data['username']
+                role_id = forms_obj.cleaned_data['role_id']
+                #  查询数据库  用户id
+                user_obj = models.UserProfile.objects.filter(
+                    id = user_id
+                )
+                #  更新 数据
+                user_obj.update(username=username,role_id=role_id)
+                print(user_obj)
+
+                response.code = 200
+                response.msg = "修改成功"
+            else:
+                print("验证不通过")
+                # print(forms_obj.errors)
+                response.code = 301
+                # print(forms_obj.errors.as_json())
+                #  字符串转换 json 字符串
+                response.msg = json.loads(forms_obj.errors.as_json())
+
+    else:
+        response.code = 402
+        response.msg = "请求异常"
+
+    return JsonResponse(response.__dict__)
