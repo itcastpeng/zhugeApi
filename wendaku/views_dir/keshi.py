ba4ced9326bb60f4ac937c20d0e95695385585b5
张聪 <18511123018@163.com>
2018-04-28 21:35:40 +0800

    将 pid_id 改为 pid__name

--- wendaku/views_dir/keshi.py
+++ wendaku/views_dir/keshi.py
@@ -8,7 +8,9 @@ import time
 import datetime
 import json
 from publickFunc.condition_com import conditionCom
-from wendaku.forms.keshi_verify import KeshiAddForm,KeshiUpdateForm,KeshiSelectForm
+from wendaku.forms.keshi_verify import KeshiAddForm, KeshiUpdateForm, KeshiSelectForm
+
+
 @csrf_exempt
 @account.is_token(models.UserProfile)
 def keshi(request):
@@ -32,17 +34,17 @@ def keshi(request):
             q = conditionCom(request, field_dict)
             print('q -->', q)
             keshi_data = []
-            keshiprofile_objs = models.Keshi.objects.select_related('pid','oper_user').filter(q).order_by(order)
+            keshiprofile_objs = models.Keshi.objects.select_related('pid', 'oper_user').filter(q).order_by(order)
 
-            for  keshi_objs in keshiprofile_objs[start_line: stop_line]:
+            for keshi_objs in keshiprofile_objs[start_line: stop_line]:
                 keshi_data.append({
                     'id': keshi_objs.id,
                     'name': keshi_objs.name,
                     'create_date': keshi_objs.create_date,
-                    'pid_id':keshi_objs.pid_id,
+                    'pid__name': keshi_objs.pid.name,
                     'oper_user__username': keshi_objs.oper_user.username,
                 })
-            print('keshi_data -->',keshi_data)
+            print('keshi_data -->', keshi_data)
             response.code = 200
             response.data = {
                 'role_data': list(keshi_data),
@@ -52,7 +54,6 @@ def keshi(request):
         response.code = 402
         response.msg = "请求异常"
     return JsonResponse(response.__dict__)
-
 
 
 @csrf_exempt
@@ -68,7 +69,7 @@ def keshi_role_oper(request, oper_type, o_id):
             form_data = {
                 'oper_user_id': oper_user_id,
                 'pid_id': pid_id,
-                'name':name,
+                'name': name,
             }
             forms_obj = KeshiAddForm(form_data)
             if forms_obj.is_valid():
@@ -94,33 +95,33 @@ def keshi_role_oper(request, oper_type, o_id):
                 response.msg = '用户ID不存在'
 
         elif oper_type == "update":
-                role_update = models.Keshi.objects.filter(id=o_id)
-                if role_update:
-                    form_data = {
-                        'user_id': o_id,
-                        'name': request.POST.get('name'),
-                        'oper_user_id': request.GET.get('user_id'),
-                        'pid_id':request.POST.get('pid_id')
-                    }
-                    forms_obj = KeshiUpdateForm(form_data)
-                    if forms_obj.is_valid():
-                        user_id = forms_obj.cleaned_data['user_id']
-                        name = forms_obj.cleaned_data['name']
-                        oper_user_id = forms_obj.cleaned_data['oper_user_id']
-                        #  查询数据库  用户id
-                        user_objs = models.Keshi.objects.filter(
-                            id=user_id
+            role_update = models.Keshi.objects.filter(id=o_id)
+            if role_update:
+                form_data = {
+                    'user_id': o_id,
+                    'name': request.POST.get('name'),
+                    'oper_user_id': request.GET.get('user_id'),
+                    'pid_id': request.POST.get('pid_id')
+                }
+                forms_obj = KeshiUpdateForm(form_data)
+                if forms_obj.is_valid():
+                    user_id = forms_obj.cleaned_data['user_id']
+                    name = forms_obj.cleaned_data['name']
+                    oper_user_id = forms_obj.cleaned_data['oper_user_id']
+                    #  查询数据库  用户id
+                    user_objs = models.Keshi.objects.filter(
+                        id=user_id
+                    )
+                    if user_objs:
+                        user_objs.update(
+                            name=name, oper_user_id=oper_user_id
                         )
-                        if user_objs:
-                            user_objs.update(
-                                name=name,oper_user_id=oper_user_id
-                            )
-                            response.code = 200
-                            response.msg = "修改成功"
-                        else:
-                            response.code = 303
-                            response.msg = json.loads(forms_obj.errors.as_json())
-                            print(response.msg)
+                        response.code = 200
+                        response.msg = "修改成功"
+                    else:
+                        response.code = 303
+                        response.msg = json.loads(forms_obj.errors.as_json())
+                        print(response.msg)
         else:
             response.code = 402
             response.msg = "请求异常"
