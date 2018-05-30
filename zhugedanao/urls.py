54d935187e3e52d084534de5d4253759629e1e1a
张聪 <18511123018@163.com>
2018-05-30 16:18:17 +0800

    1、调试微信扫码登录

--- zhugedanao/urls.py
+++ zhugedanao/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 
-from zhugedanao.views_dir import login
+from zhugedanao.views_dir import login, wechat
 
 urlpatterns = [
 
     url(r'^w_login',login.w_login),
+
+    # 微信
+    url(r'^wechat/', wechat.index),
 
 
 ]
