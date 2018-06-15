ef03119237cd56a1fa08fd873a0bf03b6dc28cc7
张聪 <18511123018@163.com>
2018-06-15 15:11:01 +0800

    调试微信扫码登录

--- zhugedanao/urls.py
+++ zhugedanao/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # url(r'^w_login',login.w_login),
 
     # 微信
-    url(r'^wechat/', wechat.index),
+    url(r'^wechat', wechat.index),
 
     # 获取用于登录的微信二维码
     url(r'^generate_qrcode', wechat.generate_qrcode),
