37628dcab8b25668a4d746c7dfd91f04d2478917
zhangju <1779038564@qq.com>
2018-07-11 21:37:22 +0800

    1、后台首页数据概览,数据指标

--- zhugeleida/views_dir/admin/home_page.py
+++ zhugeleida/views_dir/admin/home_page.py
@@ -85,57 +85,6 @@ def home_page_oper(request, oper_type):
     if request.method == "GET":
 
         if oper_type == "acount_data":
-            # user_id = request.GET.get('user_id')
-            # user_obj = models.zgld_userprofile.objects.select_related('company').filter(id=user_id)
-            #
-            # company_name = user_obj[0].company.name
-            # company_id = user_obj[0].company_id
-            #
-            # user_ids = models.zgld_userprofile.objects.select_related('company').filter(
-            #     company_id=company_id).values_list('id')
-            # user_list = []
-            # if user_ids:
-            #     for u_id in user_ids: user_list.append(u_id[0])
-            #
-            #
-            # customer_num = models.zgld_user_customer_belonger.objects.filter(user_id__in=user_list).count()  # 已获取客户数
-            #
-            # follow_customer_folowup_obj = models.zgld_user_customer_flowup.objects.filter(user_id__in=user_list,
-            #                                                                               last_follow_time__isnull=False)
-            #
-            # follow_num = 0
-            # if follow_customer_folowup_obj:
-            #     follow_id_list = []
-            #     for f_obj in follow_customer_folowup_obj:
-            #         follow_id_list.append(f_obj.id)
-            #     follow_num = models.zgld_follow_info.objects.filter(user_customer_flowup_id__in=follow_id_list).count()
-            #
-            # browse_num = models.zgld_accesslog.objects.filter(user_id__in=user_list,
-            #                                                   action=1).count()  # 浏览名片的总数(包含着保存名片)
-            #
-            # # q1 = Q()
-            # # q1.add(Q(**{'action': 1}), Q.AND)
-            # # q1.add(Q(**{'action': 6}), Q.AND)
-            # forward_num = models.zgld_accesslog.objects.filter(user_id__in=user_list,
-            #                                                    action=6).count()                             # 被转发的总数-不包括转发产品
-            # saved_total_num = models.zgld_accesslog.objects.filter(user_id__in=user_list, action=8).count()  # 保存手机号
-            # user_pop_queryset = models.zgld_userprofile.objects.filter(company_id=company_id).values_list('popularity')
-            # praise_sum = 0
-            # for i in user_pop_queryset:
-            #     praise_sum = praise_sum + i[0]   # 被点赞总数
-            #
-            # ret_data = {}
-            # ret_data['count_data'] = {
-            #     'company_name': company_name,
-            #     'username': user_obj[0].username,
-            #     'customer_num': customer_num,  # 客户总数
-            #     # 'new_add_customer': ,                 # 跟进客户数
-            #     'follow_num': follow_num,  # 跟进客户数
-            #     'browse_num': browse_num,  # 浏览总数
-            #     'forward_num': forward_num,  # 被转发的总数  -包括转发名片，但是不包括转发产品
-            #     'saved_total_num': saved_total_num,  # 被保存总数-包括保存手机号（action=8）
-            #     'praise_sum': praise_sum,    # 被点赞总数
-            # }
 
             ret_data = {}
             data = request.GET.copy()
@@ -146,55 +95,25 @@ def home_page_oper(request, oper_type):
             #昨天数据
             q2 = Q()
             now_time = datetime.now()
-            create_date__gte = (now_time - timedelta(days=1)).strftime("%Y-%m-%d")
-            stop_time = ( now_time + timedelta(days=1)).strftime("%Y-%m-%d")
+            start_time = (now_time - timedelta(days=1)).strftime("%Y-%m-%d")
+            stop_time = now_time.strftime("%Y-%m-%d")
+            q2.add(Q(**{'create_date__gte': start_time}), Q.AND)  # 大于等于
             q2.add(Q(**{'create_date__lt': stop_time}), Q.AND)
-            q2.add(Q(**{'create_date__gte': create_date__gte}), Q.AND)
-
             ret_data['yesterday_data'] = deal_search_time(data, q2)
 
+            q3 = Q()
+            start_time = (now_time - timedelta(days=7)).strftime("%Y-%m-%d")
+            stop_time = now_time.strftime("%Y-%m-%d")
+            q2.add(Q(**{'create_date__gte': start_time}), Q.AND)  # 大于等于
+            q2.add(Q(**{'create_date__lt': stop_time}), Q.AND)
+            ret_data['nearly_seven_days'] = deal_search_time(data, q3)
 
-
-            # now_time = datetime.now()
-            # create_date__gte = (now_time - timedelta(days=1)).strftime("%Y-%m-%d")
-            # stop_time = (datetime.strptime(create_date__lt, '%Y-%m-%d') + timedelta(days=1)).strftime("%Y-%m-%d")
-            # q.add(Q(**{'create_date__lt': stop_time}), Q.AND)
-            # q.add(Q(**{'create_date__gte': create_date__gte}), Q.AND)
-
-            # yesterday = now() + timedelta(days=-1)  # 昨天
-            # q1.add(Q(**{'action': 1}), Q.AND)
-            # q1.add(Q(**{'action': 6}), Q.AND)
-            # yesterday_customer_num = models.zgld_user_customer_belonger.objects.filter(user_id__in=user_list,create_date=yesterday).count()
-
-            # follow_num = 0
-            # if follow_customer_folowup_obj:
-            #     follow_id_list = []
-            #     for f_obj in follow_customer_folowup_obj:
-            #         follow_id_list.append(f_obj.id)
-            #     follow_num = models.zgld_follow_info.objects.filter(user_customer_flowup_id__in=follow_id_list,create_date=yesterday).count()   # 跟进客户数
-            #
-            # browse_num = models.zgld_accesslog.objects.filter(user_id__in=user_list,create_date=yesterday,
-            #                                                   action=1).count()  # 浏览名片的总数(包含着保存名片)
-            # forward_num = models.zgld_accesslog.objects.filter(user_id__in=user_list,create_date=yesterday,
-            #                                                    action=6).count()  # 被转发的总数-不包括转发产品
-            # saved_total_num = models.zgld_accesslog.objects.filter(user_id__in=user_list,create_date=yesterday, action=8).count()  # 保存手机号
-            # user_pop_queryset = models.zgld_userprofile.objects.filter(company_id=company_id,create_date=yesterday,).values_list('popularity')
-            # praise_sum = 0
-            # for i in user_pop_queryset:
-            #     praise_sum = praise_sum + i[0]  # 被点赞总数
-
-            # ret_data['yesterday_data'] = {
-            #     'company_name': company_name,
-            #     'username': user_obj[0].username,
-            #     'customer_num': yesterday_customer_num,  # 客户总数
-            #     # 'new_add_customer': ,                 # 跟进客户数
-            #     'follow_num': follow_num,  # 跟进客户数
-            #     'browse_num': browse_num,  # 浏览总数
-            #     'forward_num': forward_num,  # 被转发的总数  -包括转发名片，但是不包括转发产品
-            #     'saved_total_num': saved_total_num,  # 被保存总数-包括保存手机号（action=8）
-            #     'praise_sum': praise_sum,    # 被点赞总数
-            # }
-
+            q4 = Q()
+            start_time = (now_time - timedelta(days=30)).strftime("%Y-%m-%d")
+            stop_time = now_time.strftime("%Y-%m-%d")
+            q2.add(Q(**{'create_date__gte': start_time}), Q.AND)  # 大于等于
+            q2.add(Q(**{'create_date__lt': stop_time}), Q.AND)
+            ret_data['nearly_thirty_days'] = deal_search_time(data, q4)
 
             #  查询成功 返回200 状态码
             response.code = 200
@@ -204,6 +123,44 @@ def home_page_oper(request, oper_type):
 
             }
 
+        elif oper_type == "line_info":
+
+            user_id = request.GET.get('user_id')
+            user_obj = models.zgld_userprofile.objects.select_related('company').filter(id=user_id)
+            company_id = user_obj[0].company_id
+            days = request.GET.get('days')
+
+            user_ids = models.zgld_userprofile.objects.select_related('company').filter(
+                company_id=company_id).values_list('id')
+            user_list = []
+            if user_ids:
+                for u_id in user_ids: user_list.append(u_id[0])
+
+            data = request.GET.copy()
+            data['user_list'] = user_list
+
+
+            ret_data = {}
+            for day in range(days,0,-1):
+
+               now_time = datetime.now()
+               start_time = (now_time - timedelta(days=day)).strftime("%Y-%m-%d")
+               # stop_time = now_time.strftime("%Y-%m-%d")
+               data['start_time'] = start_time
+
+               ret_data[start_time] = deal_line_info(data)
+
+
+            # ret = {
+            #     'customer_num': customer_num,  # 客户总数
+            #     # 'new_add_customer': ,                 # 跟进客户数
+            #     'follow_num': follow_num,  # 跟进客户数
+            #     'browse_num': browse_num,  # 浏览总数
+            #     'forward_num': forward_num,  # 被转发的总数  -包括转发名片，但是不包括转发产品
+            #     'saved_total_num': saved_total_num,  # 被保存总数-包括保存手机号（action=8）
+            #     'praise_sum': praise_sum,  # 被点赞总数
+            # }
+            #
 
 
 
@@ -216,8 +173,6 @@ def home_page_oper(request, oper_type):
 def deal_search_time(data,q):
     user_id = data.get('user_id')
     user_obj = models.zgld_userprofile.objects.select_related('company').filter(id=user_id)
-
-    company_name = user_obj[0].company.name
     company_id = user_obj[0].company_id
 
     user_ids = models.zgld_userprofile.objects.select_related('company').filter(
@@ -229,9 +184,9 @@ def deal_search_time(data,q):
 
     customer_num = models.zgld_user_customer_belonger.objects.filter(user_id__in=user_list).filter(q).count()  # 已获取客户数
 
+
     follow_customer_folowup_obj = models.zgld_user_customer_flowup.objects.filter(user_id__in=user_list,
                                                                                   last_follow_time__isnull=False)
-
     follow_num = 0
     if follow_customer_folowup_obj:
         follow_id_list = []
@@ -255,8 +210,6 @@ def deal_search_time(data,q):
 
 
     ret =  {
-        'company_name': company_name,
-        'username': user_obj[0].username,
         'customer_num': customer_num,  # 客户总数
         # 'new_add_customer': ,                 # 跟进客户数
         'follow_num': follow_num,  # 跟进客户数
@@ -268,3 +221,47 @@ def deal_search_time(data,q):
     return  ret
 
 
+def deal_line_info(data):
+    index_type = data.get('index_type')
+    start_time = data.get('start_time')
+    user_list = data.get('user_list')
+
+    q1 = Q()
+
+    q1.add(Q(**{'create_date': start_time}), Q.AND)  # 大于等于
+
+    if index_type == 1:  # 客户总数
+        customer_num = models.zgld_user_customer_belonger.objects.filter(user_id__in=user_list).filter(
+           q1).count()  # 已获取客户数
+
+    elif index_type == 2:  # 跟进总数
+        follow_customer_folowup_obj = models.zgld_user_customer_flowup.objects.filter(user_id__in=user_list,
+                                                                                      last_follow_time__isnull=False)
+        follow_num = 0
+        if follow_customer_folowup_obj:
+            follow_id_list = []
+            for f_obj in follow_customer_folowup_obj:
+                follow_id_list.append(f_obj.id)
+            follow_num = models.zgld_follow_info.objects.filter(user_customer_flowup_id__in=follow_id_list).filter(
+                q1).count()
+
+    elif index_type == 3:  # 浏览总数
+        browse_num = models.zgld_accesslog.objects.filter(user_id__in=user_list,
+                                                          action=1).filter().count()  # 浏览名片的总数(包含着保存名片)
+
+
+    elif index_type == 4:  # 被转发总数
+        forward_num = models.zgld_accesslog.objects.filter(user_id__in=user_list,
+                                                           action=6).filter(
+            q1).count()  # 被转发的总数-不包括转发产品
+
+    elif index_type == 5:  # 被保存总数
+        saved_total_num = models.zgld_accesslog.objects.filter(user_id__in=user_list, action=8).filter(
+            q1).count()  # 保存手机号
+
+    elif index_type == 6:  # 被赞总数
+        user_pop_queryset = models.zgld_userprofile.objects.filter(company_id=company_id).filter(
+            q1).values_list('popularity')
+        praise_sum = 0
+        for i in user_pop_queryset:
+            praise_sum = praise_sum + i[0]  # 被点赞总数
