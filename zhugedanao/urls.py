4d8614f4740fdf0a2d4b79e511972ea692c8e6c6
张聪 <18511123018@163.com>
2018-07-12 19:01:38 +0800

    增加记录操作日志功能

--- zhugedanao/urls.py
+++ zhugedanao/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 
 from zhugedanao.views_dir.wechat import wechat
+from zhugedanao.views_dir import oper_log
 
 urlpatterns = [
 
@@ -16,5 +17,6 @@ urlpatterns = [
     # 获取用于登录的微信二维码
     url(r'^generate_qrcode$', wechat.generate_qrcode),
 
+    url(r'^oper_log$', oper_log.oper_log),
 
 ]
