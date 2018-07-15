7f72a5d8de9e047e2fb77aa3b1591ab36ea2a3cd
zhangju <1779038564@qq.com>
2018-07-15 16:57:02 +0800

    1、上传产品图片

--- zhugeleida/urls_dir/xiaochengxu_urls.py
+++ zhugeleida/urls_dir/xiaochengxu_urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     url(r'^mingpian/(?P<oper_type>\w+)', mingpian.mingpian_oper),
 
     #查看产品 + 查看竞价产品 + 转发竞价产品 + 咨询产品。
-    url(r'^product/(?P<oper_type>\w+)/', product.product),
+    url(r'^product/(?P<oper_type>\w+)', product.product),
     # url(r'^product/(?P<oper_type>\w+)', product.product_oper),
 
     #小程序登录认证
