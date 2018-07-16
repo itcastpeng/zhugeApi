79724a6deeb53567c2bdc017747272cdcfb25bf6
张聪 <18511123018@163.com>
2018-07-16 16:15:43 +0800

    增加统计数据

--- zhugedanao/urls.py
+++ zhugedanao/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 
 from zhugedanao.views_dir.wechat import wechat
 from zhugedanao.views_dir import oper_log
+from zhugedanao.views_dir import tongji_data
 
 urlpatterns = [
 
@@ -17,6 +18,10 @@ urlpatterns = [
     # 获取用于登录的微信二维码
     url(r'^generate_qrcode$', wechat.generate_qrcode),
 
+    # 记录使用日志
     url(r'^oper_log$', oper_log.oper_log),
+
+    # 获取统计数据
+    url(r'^tongji_data$', tongji_data.tongji_data),
 
 ]
