b876b88677ca47f3168d81f11332a06cbc0a7820
张聪 <18511123018@163.com>
2018-07-18 07:57:24 +0800

    优化上传图片分片不成功的bug

--- zhugeleida/views_dir/public/img_upload.py
+++ zhugeleida/views_dir/public/img_upload.py
@@ -59,42 +59,12 @@ def img_upload(request):
         img_data = forms_obj.cleaned_data.get('img_data')  # 文件内容
         chunk = forms_obj.cleaned_data.get('chunk')  # 第几片文件
         expanded_name = img_name.split('.')[-1]  # 扩展名
-        img_source = forms_obj.cleaned_data.get('img_source')  # user_photo 代表用户上传的照片  user_avatar 代表用户的头像。
-       
 
-        global img_save_path
+        img_name = timestamp + "_" + str(chunk) + '.' + expanded_name
 
-        if img_source == 'user_photo':
-            img_name = timestamp + "_" + str(chunk) + '.' + expanded_name
-            img_save_path = "/".join([BasePath, 'statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'user_photo','tmp' , img_name])
-            print('img_save_path -->', img_save_path)
+        img_save_path = os.path.join(BasePath, 'statics', 'zhugeleida', 'imgs', 'tmp', img_name)
 
-        elif img_source == 'user_avatar':
-            img_name = timestamp + "_" + str(chunk) + '.' + expanded_name
-            img_save_path = "/".join([BasePath, 'statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'user_avatar','tmp' , img_name])
-            print('img_save_path -->', img_save_path)
-
-        elif img_source == 'user_photo':
-            img_name = timestamp + "_" + str(chunk) + '.' + expanded_name
-            img_save_path = "/".join([BasePath, 'statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'user_photo','tmp' , img_name])
-            print('img_save_path -->', img_save_path)
-
-        elif img_source == 'cover_picture' or img_source == 'product_picture': # 后台产品里cover_picture 代表封面上传的照片  product_picture 代表产品介绍图片上传
-            img_name = timestamp + "_" + str(chunk) + '.' + expanded_name
-            img_save_path = "/".join([BasePath, 'statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'product','tmp' , img_name])
-            print('img_save_path -->', img_save_path)
-
-
-        # img_data = base64.b64decode(img_data)
-        # img_data = base64.b64decode(img_data, '-_')
-        global data
-        missing_padding = 4 - len(img_data) % 4
-        if missing_padding:
-            data += b'=' * missing_padding
-
-        img_data = base64.b64decode(data)
-
-        with open(img_save_path, 'wb') as f:
+        with open(img_save_path, 'w') as f:
             f.write(img_data)
 
         response.code = 200
@@ -107,8 +77,7 @@ def img_upload(request):
     return JsonResponse(response.__dict__)
 
 
-
-# 添加企业的产品
+# 合并图片 Form 验证
 class imgMergeForm(forms.Form):
     img_name = forms.CharField(
         error_messages={
@@ -151,78 +120,81 @@ def img_merge(request):
         img_source = forms_obj.cleaned_data.get('img_source')  # user_photo 代表用户上传的照片  user_avtor 代表用户的头像。
         print('-----img_source----->', img_source)
 
-        global img_save_path
-        global obj
+        file_dir = ''
         if img_source == 'user_photo':
-            user_id = request.GET.get('user_id')
-            img_path = "/".join(['statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'user_photo' , img_name])
-            img_save_path = "/".join([BasePath, img_path])
-            file_obj = open(img_save_path, 'ab')
-            for chunk in range(chunk_num):
-                file_name = timestamp + "_" + str(chunk) + '.' + expanded_name
-
-                file_save_path = "/".join([BasePath, 'statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'user_photo', 'tmp' , file_name])
-
-                with open(file_save_path, 'rb') as f:
-                    file_obj.write(f.read())
-                    # file_content += f.read()
-                os.remove(file_save_path)
-                obj = models.zgld_user_photo.objects.create(user_id=user_id,photo_url=img_path,photo_type=1)  # 用户上传照片
-                response.data = {
-                    'picture_id': obj.id,
-                    'picture_url': obj.photo_url,
-                }
+            file_dir = os.path.join('statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'user_photo')
+            #
+            # user_id = request.GET.get('user_id')
+            # img_path = "/".join(['statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'user_photo' , img_name])
+            # img_save_path = "/".join([BasePath, img_path])
+            # file_obj = open(img_save_path, 'ab')
+            # for chunk in range(chunk_num):
+            #     file_name = timestamp + "_" + str(chunk) + '.' + expanded_name
+            #
+            #     file_save_path = "/".join([BasePath, 'statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'user_photo', 'tmp' , file_name])
+            #
+            #     with open(file_save_path, 'rb') as f:
+            #         file_obj.write(f.read())
+            #         # file_content += f.read()
+            #     os.remove(file_save_path)
+            #     obj = models.zgld_user_photo.objects.create(user_id=user_id,photo_url=img_path,photo_type=1)  # 用户上传照片
+            #     response.data = {
+            #         'picture_id': obj.id,
+            #         'picture_url': obj.photo_url,
+            #     }
 
         elif img_source == 'user_avatar':
-            user_id = request.GET.get('user_id')
-            img_path = "/".join(['statics', 'zhugeleida', 'imgs','qiyeweixin', 'user_avatar' , img_name])
-            img_save_path = "/".join([BasePath, img_path])
-            file_obj = open(img_save_path, 'ab')
-            for chunk in range(chunk_num):
-                file_name = timestamp + "_" + str(chunk) + '.' + expanded_name
+            file_dir = os.path.join('statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'user_avatar')
 
-                file_save_path = "/".join(
-                    [BasePath, 'statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'user_avatar','tmp', file_name])
+        elif img_source == 'cover_picture' or img_source == 'product_picture':
+            file_dir = os.path.join('statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'product')
 
-                with open(file_save_path, 'rb') as f:
-                    file_obj.write(f.read())
+            # user_id = request.GET.get('user_id')
+            # img_path = "/".join(['statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'product', img_name])
+            # img_save_path = "/".join([BasePath, img_path])
+            # file_obj = open(img_save_path, 'ab')
+            # for chunk in range(chunk_num):
+            #     file_name = timestamp + "_" + str(chunk) + '.' + expanded_name
+            #
+            #     file_save_path = "/".join(
+            #         [BasePath, 'statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'product', 'tmp', file_name])
+            #
+            #     with open(file_save_path, 'rb') as f:
+            #         file_obj.write(f.read())
+            #
+            #     os.remove(file_save_path)
+            #
+            #     if img_source == 'cover_picture':
+            #         obj = models.zgld_product_picture.objects.create(picture_url=img_path,
+            #                                                          picture_type=1)  # 封面头像
+            #     elif  img_source == 'product_picture':
+            #         obj = models.zgld_product_picture.objects.create(picture_url=img_path,
+            #                                                          picture_type=2)  # 产品图片
+            #     response.data = {
+            #         'picture_id': obj.id,
+            #         'picture_url': obj.picture_url,
+            #     }
 
-                os.remove(file_save_path)
-                obj = models.zgld_user_photo.objects.create(user_id=user_id, photo_url=img_path,photo_type=2)   # 用户名片头像
+        fileData = ''
+        for chunk in range(chunk_num):
+            file_name = timestamp + "_" + str(chunk) + '.' + expanded_name
+            file_save_path = os.path.join(BasePath, 'statics', 'zhugeleida', 'imgs', 'tmp', file_name)
+            with open(file_save_path, 'r') as f:
+                fileData += f.read()
 
-                response.data = {
-                    'picture_id': obj.id,
-                    'picture_url': obj.photo_url,
-                }
+            os.remove(file_save_path)
 
+        # user_id = request.GET.get('user_id')
+        img_path = os.path.join(file_dir, img_name)
+        img_save_path = os.path.join(BasePath, img_path)
+        file_obj = open(img_save_path, 'ab')
+        img_data = base64.b64decode(fileData)
+        file_obj.write(img_data)
+        # obj = models.zgld_user_photo.objects.create(user_id=user_id, photo_url=img_path,photo_type=2)   # 用户名片头像
 
-        elif  img_source == 'cover_picture' or img_source == 'product_picture':
-            user_id = request.GET.get('user_id')
-            img_path = "/".join(['statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'product', img_name])
-            img_save_path = "/".join([BasePath, img_path])
-            file_obj = open(img_save_path, 'ab')
-            for chunk in range(chunk_num):
-                file_name = timestamp + "_" + str(chunk) + '.' + expanded_name
-
-                file_save_path = "/".join(
-                    [BasePath, 'statics', 'zhugeleida', 'imgs', 'qiyeweixin', 'product', 'tmp', file_name])
-
-                with open(file_save_path, 'rb') as f:
-                    file_obj.write(f.read())
-
-                os.remove(file_save_path)
-
-                if img_source == 'cover_picture':
-                    obj = models.zgld_product_picture.objects.create(picture_url=img_path,
-                                                                     picture_type=1)  # 封面头像
-                elif  img_source == 'product_picture':
-                    obj = models.zgld_product_picture.objects.create(picture_url=img_path,
-                                                                     picture_type=2)  # 产品图片
-                response.data = {
-                    'picture_id': obj.id,
-                    'picture_url': obj.picture_url,
-                }
-
+        response.data = {
+            'picture_url': img_path,
+        }
         response.code = 200
         response.msg = "添加图片成功"
 
