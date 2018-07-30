469cb81981cb7743d45bb101a399d310b89f0cc7
zhangju <1779038564@qq.com>
2018-07-30 12:50:55 +0800

    1、更新models

--- zhugeleida/models.py
+++ zhugeleida/models.py
@@ -542,7 +542,7 @@ class zgld_article(models.Model):
     user = models.ForeignKey('zgld_userprofile',verbose_name='文章作者',null=True)
     # category = models.ForeignKey(verbose_name='文章类型', to='Category', to_field='nid', null=True)
 
-    tags = models.ManyToManyField('zgld_article_tag',through='zgld_article_to_tag',through_fields=('article', 'tag'))
+    # tags = models.ManyToManyField('zgld_article_tag',through='zgld_article_to_tag',through_fields=('article', 'tag'))
     # up_count = models.IntegerField(default=0,verbose_name="赞次数")
     # down_count = models.IntegerField(default=0,verbose_name="踩次数")
     cover_picture  = models.CharField(verbose_name="封面图片URL",max_length=128)
@@ -566,16 +566,16 @@ class zgld_article_tag(models.Model):
         app_label = "zhugeleida"
 
 ##文章和标签绑定关系表
-class zgld_article_to_tag(models.Model):
-    article = models.ForeignKey('zgld_article',verbose_name='文章',)
-    tag =  models.ForeignKey('zgld_article_tag',verbose_name='文章标签')
-
-    class Meta:
-        unique_together = [
-            ('article', 'tag'),
-        ]
-        verbose_name_plural = "文章和标签关系绑定表"
-        app_label = "zhugeleida"
+# class zgld_article_to_tag(models.Model):
+#     article = models.ForeignKey('zgld_article',verbose_name='文章',)
+#     tag =  models.ForeignKey('zgld_article_tag',verbose_name='文章标签')
+#
+#     class Meta:
+#         unique_together = [
+#             ('article', 'tag'),
+#         ]
+#         verbose_name_plural = "文章和标签关系绑定表"
+#         app_label = "zhugeleida"
 
 
 # 文章查看用户停留时间表
