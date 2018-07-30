a6cb38b0b7bb4bf78c8250f8a7659b849eb2b962
zhangju <1779038564@qq.com>
2018-07-30 12:31:34 +0800

    1、更新models

--- zhugeleida/models.py
+++ zhugeleida/models.py
@@ -528,7 +528,6 @@ class zgld_chatinfo(models.Model):
 
 # 文章表
 class zgld_article(models.Model):
-    user = models.ForeignKey('zgld_admin_userprofile', verbose_name='文章作者', null=True)
     title = models.CharField(verbose_name='文章标题', max_length=128)
     summary = models.CharField(verbose_name='文章摘要', max_length=255)
     status_choices = ( (1,'已发'),
@@ -540,9 +539,10 @@ class zgld_article(models.Model):
                      )
     source = models.SmallIntegerField(default=1, verbose_name='文章来源', choices=source_choices)
     content = models.TextField(verbose_name='文章内容', null=True)
+    user = models.ForeignKey('zgld_userprofile',verbose_name='文章作者',null=True)
+    # category = models.ForeignKey(verbose_name='文章类型', to='Category', to_field='nid', null=True)
 
-
-    tags = models.ManyToManyField('zgld_article_tag',verbose_name="文章关联的标签")
+    tags = models.ManyToManyField('zgld_article_tag',through='zgld_article_to_tags',through_fields=('article', 'tag'))
     # up_count = models.IntegerField(default=0,verbose_name="赞次数")
     # down_count = models.IntegerField(default=0,verbose_name="踩次数")
     cover_picture  = models.CharField(verbose_name="封面图片URL",max_length=128)
@@ -555,7 +555,6 @@ class zgld_article(models.Model):
         verbose_name_plural = "文章表"
         app_label = "zhugeleida"
 
-
 #文章标签表
 class zgld_article_tag(models.Model):
     user = models.ForeignKey('zgld_userprofile',verbose_name="标签所属用户",null=True)
@@ -566,6 +565,18 @@ class zgld_article_tag(models.Model):
         verbose_name_plural = "文章标签表"
         app_label = "zhugeleida"
 
+
+# 文章和标签绑定关系表
+class zgld_article_to_tags(models.Model):
+    article = models.ForeignKey('zgld_article',verbose_name='文章',)
+    tag =  models.ForeignKey('zgld_article_tag',verbose_name='文章标签')
+
+    class Meta:
+        unique_together = [
+            ('article', 'tag'),
+        ]
+        verbose_name_plural = "文章和标签关系绑定表"
+        app_label = "zhugeleida"
 
 
 # 文章查看用户停留时间表
