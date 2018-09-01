276d02df48a8d0e66039136a6935659c0f7d68d4
zhangju <1779038564@qq.com>
2018-09-01 16:23:41 +0800

    生成公众号分享链接URL

--- zhugeleida/views_dir/gongzhonghao/user_gongzhonghao_auth.py
+++ zhugeleida/views_dir/gongzhonghao/user_gongzhonghao_auth.py
@@ -313,7 +313,7 @@ def user_gongzhonghao_auth_oper(request,oper_type):
             '''
 
             company_id = request.GET.get('company_id')
-            print('------- 公众号 签名 request.GET--------->',request.GET)
+            print('------- 公众号 签名算法 request.GET--------->',request.GET)
             rc = redis.StrictRedis(host='redis_host', port=6379, db=8, decode_responses=True)
 
             objs = models.zgld_gongzhonghao_app.objects.filter(id=company_id)
@@ -378,6 +378,7 @@ def user_gongzhonghao_auth_oper(request,oper_type):
                     'nonceStr': noncestr,
                     'appid': authorizer_appid
                 }
+                print('------- 公众号 签名算法 response.data--------->', response.data)
 
             else:
                 response.code = 301
