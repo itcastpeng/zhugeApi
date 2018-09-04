05b6eb929595d213293ccd7308ee680949ce3313
张聪 <18511123018@163.com>
2018-09-04 09:58:16 +0800

    增加boos雷达路由

---
+++ zhugeleida/urls_dir/boosleida_urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+
+from zhugeleida.views_dir.boosleida import boosleida
+
+urlpatterns = [
+    # 文章的标签管理
+    # url(r'^article_tag/(?P<oper_type>\w+)/(?P<o_id>\d+)$', article_tag.article_tag_oper),
+    # url(r'^article_tag$', article_tag.article_tag),
+
+    # boos雷达数据统计
+    # url(r'^article_tag/(?P<oper_type>\w+)/(?P<o_id>\d+)$', article_tag.article_tag_oper),
+    # url(r'^$', ),
+
+
+]
