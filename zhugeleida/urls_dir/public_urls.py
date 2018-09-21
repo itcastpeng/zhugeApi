e8d00f860f44d265bc3498c5dc3ba5675568d01e
张聪 <18511123018@163.com>
2018-09-21 10:42:44 +0800

    增加文件上传的接口

--- zhugeleida/urls_dir/public_urls.py
+++ zhugeleida/urls_dir/public_urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from zhugeleida.views_dir.public import img_upload
+from zhugeleida.views_dir.public import img_upload, fild_upload
 
 urlpatterns = [
     url(r'^img_upload$', img_upload.img_upload),
     url(r'^ueditor_img_upload$', img_upload.ueditor_image_upload),
     url(r'^img_merge$', img_upload.img_merge),
+    url(r'^fild_upload$', fild_upload.fild_upload),
 
 ]
