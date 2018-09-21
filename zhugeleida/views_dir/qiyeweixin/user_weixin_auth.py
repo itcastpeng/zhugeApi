4154235b8a56660a02cd74d68aab7f1b100ba340
zhangju <1779038564@qq.com>
2018-09-21 11:58:54 +0800

    1\

--- zhugeleida/views_dir/qiyeweixin/user_weixin_auth.py
+++ zhugeleida/views_dir/qiyeweixin/user_weixin_auth.py
@@ -108,12 +108,16 @@ def work_weixin_auth(request, company_id):
 
                 redirect_url = 'http://zhugeleida.zhugeyingxiao.com?token=' + user_profile_obj.token + '&' + 'id=' + str(
                     user_profile_obj.id) + '&' + 'avatar=' + avatar
+
+                print('----------【雷达用户】存在且《登录成功》，user_id | userid | redirect_url ---->', user_profile_obj.id, "|",
+                      userid, "\n", redirect_url)
+
                 return redirect(redirect_url)
 
-            print('----------【雷达用户】存在且《登录成功》，user_id | userid | redirect_url ---->', user_profile_obj.id, "|", userid,"\n",redirect_url)
 
-        print('----------【雷达用户】不存在 ,未登录成功 userid | company_id ------>',userid,company_id)
-        return redirect('http://zhugeleida.zhugeyingxiao.com/err_page')
+        else:
+            print('----------【雷达用户】不存在 ,未登录成功 userid | company_id ------>',userid,company_id)
+            return redirect('http://zhugeleida.zhugeyingxiao.com/err_page')
 
     else:
         response.code = 402
