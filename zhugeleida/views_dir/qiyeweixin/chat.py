8b4bb8c81e26ffbc6290dc4a9c0024099a0ecb81
zhangju <1779038564@qq.com>
2018-10-08 12:47:34 +0800

    1、小程序二维码

--- zhugeleida/views_dir/qiyeweixin/chat.py
+++ zhugeleida/views_dir/qiyeweixin/chat.py
@@ -300,6 +300,7 @@ def chat_oper(request, oper_type, o_id):
                     tasks.user_send_template_msg_to_customer.delay(json.dumps(data))  # 发送【小程序】模板消息
 
                 elif  user_type == 1 and customer_id and user_id:
+                    print('--- 【公众号发送模板消息】 user_send_gongzhonghao_template_msg --->',json.dumps(data))
                     data['customer_id'] = customer_id
                     data['user_id'] = user_id
                     tasks.user_send_gongzhonghao_template_msg.delay(json.dumps(data))
