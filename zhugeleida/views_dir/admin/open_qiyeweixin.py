845b44e1696838c30630cb5707db5cf53d5c6ea0
zhangju <1779038564@qq.com>
2018-10-09 12:57:13 +0800

    1、小程序二维码

--- zhugeleida/views_dir/admin/open_qiyeweixin.py
+++ zhugeleida/views_dir/admin/open_qiyeweixin.py
@@ -30,42 +30,42 @@ def  open_qiyeweixin(request, oper_type):
             nonce = request.GET.get('nonce')
             echostr = request.GET.get('echostr')
 
-            if not echostr: #没有这个字段说明不是获取用户事件回调的验证URL
-
-                postdata = request.body.decode(encoding='UTF-8')
-
-                sToken = "5lokfwWTqHXnb58VCV"
-                sEncodingAESKey = "ee2taRqANMUsH7JIhlSWIj4oeGAJG08qLCAXNf6HCxt"
-                sCorpID = "wx5d26a7a856b22bec"
-                decrypt_obj = WXBizMsgCrypt_qiyeweixin.WXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID)
 
 
-                # xml_tree = ET.fromstring(postdata)
-                # msg_signature = "2b29a5534ed8b50981ae0069c1f4c48127789cec"
-                # timestamp = "1538228121"
-                # nonce = "1537294388"
-                # sReqData = '<xml><ToUserName><![CDATA[wx5d26a7a856b22bec]]></ToUserName><Encrypt><![CDATA[uh2c6Yqs5f8nPcXQmTtxifpYEIX0Y5FV/nrsbAo4FGKdCPCLVA1p7XSDnC6XN5/1YiFE4ywFs2CvT0n1xHbJ4vSksICKqkPr0z9PtxhJDcbuhz7wgsUSLmEMeXWR1f6YVaOGkFKqa6YJ0lalvpGcS03RRwTuqb49VccfuV5KO4y3eabi6qQRh5QG6SHYKGPZmTfD32Q5GGgGhm4QH3ne/hUTLtMdk3CONblGcodRs5/iAArxfGCFxYADT9d/9Q6ZoNoLruYD66RPrX8AghjKE6KoCqNomsgLHbINJEBxkyEaTBd9qqJe+zoXJMXhyFJ6CsmfKRITwC/Lz32wZF0bF44fzhybguIyMOohxZEhyl1pJpwpgX5DjpjKs47jKf76]]></Encrypt><AgentID><![CDATA[]]></AgentID></xml>'
+            postdata = request.body.decode(encoding='UTF-8')
 
-                ret, sMsg = decrypt_obj.DecryptMsg(postdata, msg_signature, timestamp, nonce)
-                print(ret, sMsg)
-                if (ret != 0):
-                    print("--- 企业微信解密 ERR: DecryptMsg ret --->: " + str(ret))
-                    sys.exit(1)
-
-                # 解密成功，sMsg即为xml格式的明文
-                xml_tree = ET.fromstring(sMsg)
-                SuiteTicket = xml_tree.find("SuiteTicket").text
-                SuiteId = xml_tree.find("SuiteId").text
-
-                rc = redis.StrictRedis(host='redis_host', port=6379, db=8, decode_responses=True)
-
-                key_name = 'SuiteTicket_%s' % (SuiteId)
-                rc.set(key_name, SuiteTicket, 3000)
-                print('--------企业微信服务器 SuiteId | suite_ticket--------->>', SuiteId, '|', SuiteTicket)
+            sToken = "5lokfwWTqHXnb58VCV"
+            sEncodingAESKey = "ee2taRqANMUsH7JIhlSWIj4oeGAJG08qLCAXNf6HCxt"
+            sCorpID = "wx5d26a7a856b22bec"
+            decrypt_obj = WXBizMsgCrypt_qiyeweixin.WXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID)
 
 
+            # xml_tree = ET.fromstring(postdata)
+            # msg_signature = "2b29a5534ed8b50981ae0069c1f4c48127789cec"
+            # timestamp = "1538228121"
+            # nonce = "1537294388"
+            # sReqData = '<xml><ToUserName><![CDATA[wx5d26a7a856b22bec]]></ToUserName><Encrypt><![CDATA[uh2c6Yqs5f8nPcXQmTtxifpYEIX0Y5FV/nrsbAo4FGKdCPCLVA1p7XSDnC6XN5/1YiFE4ywFs2CvT0n1xHbJ4vSksICKqkPr0z9PtxhJDcbuhz7wgsUSLmEMeXWR1f6YVaOGkFKqa6YJ0lalvpGcS03RRwTuqb49VccfuV5KO4y3eabi6qQRh5QG6SHYKGPZmTfD32Q5GGgGhm4QH3ne/hUTLtMdk3CONblGcodRs5/iAArxfGCFxYADT9d/9Q6ZoNoLruYD66RPrX8AghjKE6KoCqNomsgLHbINJEBxkyEaTBd9qqJe+zoXJMXhyFJ6CsmfKRITwC/Lz32wZF0bF44fzhybguIyMOohxZEhyl1pJpwpgX5DjpjKs47jKf76]]></Encrypt><AgentID><![CDATA[]]></AgentID></xml>'
 
-                return HttpResponse("success")
+            ret, sMsg = decrypt_obj.DecryptMsg(postdata, msg_signature, timestamp, nonce)
+            print(ret, sMsg)
+            if (ret != 0):
+                print("--- 企业微信解密 ERR: DecryptMsg ret --->: " + str(ret))
+                sys.exit(1)
+
+            # 解密成功，sMsg即为xml格式的明文
+            xml_tree = ET.fromstring(sMsg)
+            SuiteTicket = xml_tree.find("SuiteTicket").text
+            SuiteId = xml_tree.find("SuiteId").text
+
+            rc = redis.StrictRedis(host='redis_host', port=6379, db=8, decode_responses=True)
+
+            key_name = 'SuiteTicket_%s' % (SuiteId)
+            rc.set(key_name, SuiteTicket, 3000)
+            print('--------企业微信服务器 SuiteId | suite_ticket--------->>', SuiteId, '|', SuiteTicket)
+
+
+
+            return HttpResponse("success")
 
 
 
@@ -88,29 +88,7 @@ def  open_qiyeweixin(request, oper_type):
             }
             # 授权成功，返回临时授权码;第三方服务商需尽快使用临时授权码换取永久授权码及授权信息
 
-        elif oper_type == 'callback_data':
 
-            msg_signature = request.GET.get('msg_signature')
-            timestamp = request.GET.get('timestamp')
-            nonce = request.GET.get('nonce')
-            echostr = request.GET.get('echostr')
-
-            sToken = "5lokfwWTqHXnb58VCV"
-            sEncodingAESKey = "ee2taRqANMUsH7JIhlSWIj4oeGAJG08qLCAXNf6HCxt"
-            # sCorpID = "wx5d26a7a856b22bec"
-            sCorpID = "wx81159f52aff62388"
-            wxcpt = WXBizMsgCrypt_qiyeweixin.WXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID)
-
-
-            # ret, sMsg = decrypt_obj.DecryptMsg(postdata, msg_signature, timestamp, nonce)
-            ret, sEchoStr = wxcpt.VerifyURL(msg_signature, timestamp, nonce, echostr)
-            if (ret != 0):
-                print("---- 验证回调URL: VerifyURL ret: ----> " + str(ret))
-                sys.exit(1)
-            print('----- [callback_data]解密echostr参数得到消息内容 -------->>',sEchoStr)
-
-            # 验证URL成功，将sEchoStr返回给企业号
-            return HttpResponse(sEchoStr)
 
 
         # 设置授权配置 /zhugeleida/admin/open_qiyeweixin/set_session_info
@@ -169,7 +147,31 @@ def  open_qiyeweixin(request, oper_type):
             # 验证URL成功，将sEchoStr返回给企业号
             return HttpResponse(sEchoStr)
 
+        elif oper_type == 'callback_data':
 
+            msg_signature = request.GET.get('msg_signature')
+            timestamp = request.GET.get('timestamp')
+            nonce = request.GET.get('nonce')
+            echostr = request.GET.get('echostr')
+
+            sToken = "5lokfwWTqHXnb58VCV"
+            sEncodingAESKey = "ee2taRqANMUsH7JIhlSWIj4oeGAJG08qLCAXNf6HCxt"
+            # sCorpID = "wx5d26a7a856b22bec"
+            sCorpID = "wx81159f52aff62388"
+            wxcpt = WXBizMsgCrypt_qiyeweixin.WXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID)
+
+
+            # ret, sMsg = decrypt_obj.DecryptMsg(postdata, msg_signature, timestamp, nonce)
+            ret, sEchoStr = wxcpt.VerifyURL(msg_signature, timestamp, nonce, echostr)
+            if (ret != 0):
+                print("---- 验证回调URL: VerifyURL ret: ----> " + str(ret))
+                sys.exit(1)
+            print('----- [callback_data]解密echostr参数得到消息内容 -------->>',sEchoStr)
+
+            # 验证URL成功，将sEchoStr返回给企业号
+            return HttpResponse(sEchoStr)
+
+        
     return JsonResponse(response.__dict__)
 
 
