248b88ee84622b2f480131b343a6e8f9d8288d65
zhangju <1779038564@qq.com>
2018-10-13 17:37:18 +0800

    话术库修复

--- zhugeleida/urls_dir/admin_urls.py
+++ zhugeleida/urls_dir/admin_urls.py
@@ -35,8 +35,8 @@ urlpatterns = [
 
 
     # 微信公众号通知+消息与事件接收
+    url(r'^open_weixin_gongzhonghao/(?P<oper_type>\w+)/(?P<app_id>\w+)$', open_weixin_gongzhonghao.open_weixin_gongzhonghao_oper),
     url(r'^open_weixin_gongzhonghao/(?P<oper_type>\w+)$', open_weixin_gongzhonghao.open_weixin_gongzhonghao),
-    url(r'^open_weixin_gongzhonghao/(?P<oper_type>\w+)/(?P<app_id>\w+)$', open_weixin_gongzhonghao.open_weixin_gongzhonghao),
 
     # 小程序第三方进入认证
     url(r'^xcx_auth_process/update/(?P<oper_type>\w+)$', open_weixin.xcx_auth_process_oper),
