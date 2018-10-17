0c5a3491e83ca6545279a58138fa2cc0935a1a56
zhangju <1779038564@qq.com>
2018-10-17 15:51:17 +0800

    文章转发红包

--- zhugeleida/Test_bak/Test_gongzhonghao.py
+++ zhugeleida/Test_bak/Test_gongzhonghao.py
@@ -356,11 +356,19 @@ ip = 'http://api.zhugeyingxiao.com'
 # ret = requests.get(url, data = post_data ,params=get_data)
 
 
-url =  ip + '/zhugeleida/mycelery/user_forward_send_activity_redPacket'  # 获取产品的列表
-get_data['parent_id'] = 850
-get_data['article_id'] = 23
-get_data['activity_id'] = 2
-get_data['company_id'] = 1
+# url =  ip + '/zhugeleida/mycelery/user_forward_send_activity_redPacket'  # 获取产品的列表
+# get_data['parent_id'] = 850
+# get_data['article_id'] = 23
+# get_data['activity_id'] = 2
+# get_data['company_id'] = 1
+# ret = requests.get(url, data = post_data ,params=get_data)
+
+
+url =  ip + '/zhugeleida/gongzhonghao/article/test_send_redPacket/0'  # 获取产品的列表
+# get_data['parent_id'] = 850
+# get_data['article_id'] = 23
+# get_data['activity_id'] = 2
+# get_data['company_id'] = 1
 ret = requests.get(url, data = post_data ,params=get_data)
 
 
