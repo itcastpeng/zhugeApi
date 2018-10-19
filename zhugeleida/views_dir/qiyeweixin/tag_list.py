73de4317109ae9fbab5a7d419be97396325c2a60
zhangju <1779038564@qq.com>
2018-10-19 17:19:16 +0800

    bug

--- zhugeleida/views_dir/qiyeweixin/tag_list.py
+++ zhugeleida/views_dir/qiyeweixin/tag_list.py
@@ -28,7 +28,7 @@ def tag_list(request):
 
 
 
-        tag_values = models.zgld_tag.objects.filter(Q(user_id=user_id) | Q(user_id__isnull=True)).values_list('id', 'name', 'tag_parent_id')
+        tag_values = models.zgld_tag.objects.filter(Q(user_id=user_id) | Q(user_id__isnull=True)).values_list('id', 'name', 'tag_parent_id').order_by('-create_date')
         tag_dict = {}
         ret_data = []
         date_list = list(tag_values)
