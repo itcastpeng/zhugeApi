5af78033f111dfbf3f3349b98712b00699206b8a
zhangju <1779038564@qq.com>
2018-10-24 20:04:44 +0800

    发送客户消息

--- zhugeleida/views_dir/mycelery/mycelery.py
+++ zhugeleida/views_dir/mycelery/mycelery.py
@@ -784,11 +784,13 @@ def user_send_gongzhonghao_template_msg(request):
             kefu_msg_get_data = {
                 'access_token': authorizer_access_token,
             }
+
+            content =  json.dumps(content)
             msg = content.get('msg')
             msg = base64.b64decode(msg)
             msg = str(msg, 'utf-8')
 
-            
+
             _content  = '%s:\n        %s' % (user_name,msg)
             kefu_msg_post_data ={
                 "touser": openid,
