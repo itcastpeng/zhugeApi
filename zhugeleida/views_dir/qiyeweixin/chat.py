6b7f127a05678f3df327e78ac54141d1af5d8596
zhangju <1779038564@qq.com>
2018-11-06 16:23:00 +0800

    帮助文档

--- zhugeleida/views_dir/qiyeweixin/chat.py
+++ zhugeleida/views_dir/qiyeweixin/chat.py
@@ -248,6 +248,15 @@ def chat_oper(request, oper_type, o_id):
                     if not content:
                         continue
 
+                    is_customer_new_msg = obj.is_customer_new_msg
+
+                    if is_customer_new_msg:  # 为True时
+                        is_customer_already_read = 0  # 未读
+                        is_customer_already_read_text = '未读'
+                    else:
+                        is_customer_already_read = 1  # 已读
+                        is_customer_already_read_text = '已读'
+
                     _content = json.loads(content)
                     info_type = _content.get('info_type')
                     if info_type:
@@ -266,7 +275,9 @@ def chat_oper(request, oper_type, o_id):
                         'src': obj.customer.headimgurl,
                         'name': customer_name,
                         'dateTime': obj.create_date,
-                        'send_type': obj.send_type
+                        'send_type': obj.send_type,
+                        'is_customer_already_read': is_customer_already_read,
+                        'is_customer_already_read_text': is_customer_already_read_text
                     }
                     base_info_dict.update(_content)
 
