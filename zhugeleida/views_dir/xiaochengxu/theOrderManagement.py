a7c4d24191d660a8f9007f761283dd302537f3f7
zhangju <1779038564@qq.com>
2018-12-07 10:38:40 +0800

    雷达socket

--- zhugeleida/views_dir/xiaochengxu/theOrderManagement.py
+++ zhugeleida/views_dir/xiaochengxu/theOrderManagement.py
@@ -178,24 +178,19 @@ def theOrderOper(request, oper_type, o_id):
                     orderObjs.update(logicDelete=1)
                     response.code = 200
                     response.msg = '删除成功'
-                elif status in [8, 9]:
+                elif status in [2, 3, 8, 9]:
                     tuiKuanObjs = models.zgld_shangcheng_tuikuan_dingdan_management.objects.filter(orderNumber_id=o_id)
                     if tuiKuanObjs:
-                        
-                        if status in [2, 3]:
-                            orderObjs.update(logicDelete=1)
-                            tuiKuanObjs.update(logicDelete=1)
+                        orderObjs.update(logicDelete=1)
+                        tuiKuanObjs.update(logicDelete=1)
 
-                        else:
-                            response.code = 301
-                            response.msg = '该订单有退款业务, 不可删除！'
                     else:
                         orderObjs.update(logicDelete=1)
                         response.code = 200
                         response.msg = '删除成功'
                 else:
                     response.code = 301
-                    response.msg = '删除失败, 当前状态不可删除'
+                    response.msg = '删除失败,当前状态不可删除'
             else:
                 response.code = 301
                 response.msg = '无此订单'
