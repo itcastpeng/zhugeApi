5cde4a38ae1432bde994605afc8fe21d0b9d9c21
zhangju <1779038564@qq.com>
2018-12-19 14:09:06 +0800

    AI报表

--- zhugeleida/urls_dir/qiyeweixin_urls.py
+++ zhugeleida/urls_dir/qiyeweixin_urls.py
@@ -58,8 +58,6 @@ urlpatterns = [
 
     url(r'^websocket/(?P<oper_type>\w+)$', websocket.leida_websocket),
 
-    # url(r'^boosleida/(?P<oper_type>\w+)$',  ),
-
 
     # 获取访问日志动作。
     url(r'^action/(?P<oper_type>\w+)$', action.action),
