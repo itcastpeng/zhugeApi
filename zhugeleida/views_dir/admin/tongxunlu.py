9d7796282b7f3f51097bb28d1bf147bbad378a04
zhangju <1779038564@qq.com>
2018-12-19 09:31:16 +0800

    客户管理

--- zhugeleida/views_dir/admin/tongxunlu.py
+++ zhugeleida/views_dir/admin/tongxunlu.py
@@ -348,12 +348,12 @@ def tongxunlu_oper(request, oper_type):
                 ## 工作交接
                 if type == 'all_customer':
                     customer_objs = models.zgld_user_customer_belonger.objects.select_related('user').filter(
-                        userprofile_id=old_uid,user__company_id=company_id)
+                        user_id=old_uid,user__company_id=company_id)
 
                     for obj in customer_objs:
 
                         _customer_id =obj.customer_id
-                        validate_customer_objs = models.zgld_user_customer_belonger.objects.filter(userprofile_id=new_uid,customer_id=_customer_id)
+                        validate_customer_objs = models.zgld_user_customer_belonger.objects.filter(user_id=new_uid,customer_id=_customer_id)
                         chat_objs = models.zgld_chatinfo.objects.filter(customer_id=_customer_id, userprofile_id=old_uid)  #
 
                         if validate_customer_objs:
@@ -387,7 +387,7 @@ def tongxunlu_oper(request, oper_type):
                         for obj in customer_objs:
 
                             _customer_id = obj.customer_id
-                            validate_customer_objs = models.zgld_user_customer_belonger.objects.filter(userprofile_id=new_uid,
+                            validate_customer_objs = models.zgld_user_customer_belonger.objects.filter(user_id=new_uid,
                                                                                                        customer_id=_customer_id) ##
 
                             chat_objs = models.zgld_chatinfo.objects.filter(customer_id=_customer_id, userprofile_id=old_uid) #
