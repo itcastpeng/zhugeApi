2e8ef51039dabcd228e20d2f08fac43f694fb830
zhangju <1779038564@qq.com>
2018-12-24 20:21:31 +0800

    小程序商城分类

--- zhugeleida/views_dir/admin/shangchengjichushezhi.py
+++ zhugeleida/views_dir/admin/shangchengjichushezhi.py
@@ -166,13 +166,13 @@ def jiChuSheZhiOper(request, oper_type):
                         re_openid = customer_obj.openid
 
                     result_data = {
-                        # 'scene_id' : 'PRODUCT_1', #   发放红包使用场景，红包金额大于200或者小于1元时必传
+                        'scene_id' : 'PRODUCT_1', #   发放红包使用场景，红包金额大于200或者小于1元时必传
                         'nonce_str': yuzhifu.generateRandomStamping(),  # 32位随机值a
                         'mch_billno': dingdanhao,  # 订单号
                         'mch_id': shangHuHao,      # 商户号
                         'wxappid': appid,  # 真实数据appid
                         're_openid': re_openid,  # 用户唯一标识
-                        'total_amount': 100,  # 付款金额 1:100 | 发送一分钱
+                        'total_amount': 1,  # 付款金额 1:100 | 发送一分钱
                         'client_ip': '192.168.1.1',  # 终端IP
                         'total_num': 1,  # 红包发放总人数
                         'send_name': '诸葛雷达_测试发红包',  # 商户名称 中文
@@ -211,6 +211,7 @@ def jiChuSheZhiOper(request, oper_type):
                         return_code = collection.getElementsByTagName("return_msg")[0].childNodes[0].data
                         response.code = '%s' % (return_code)
                         response.msg = '微信接口返回错误: {return_msg}'.format(return_msg=return_msg)
+
                         return JsonResponse(response.__dict__)
                 else:
                     response.code = 301
