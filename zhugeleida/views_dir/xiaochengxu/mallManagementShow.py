791f6f9cd48d0d9af2a4fd12feccdb80536309b0
zhangju <1779038564@qq.com>
2018-12-24 16:52:12 +0800

    商城详情模块化

--- zhugeleida/views_dir/xiaochengxu/mallManagementShow.py
+++ zhugeleida/views_dir/xiaochengxu/mallManagementShow.py
@@ -12,119 +12,125 @@ import json, base64
 @csrf_exempt
 @account.is_token(models.zgld_customer)
 def mallManage(request):
-    # response = mallManagement.mallManagement(request, uid, goodsGroup, status, flag)
+
+
     response = Response.ResponseObj()
-    uid = request.GET.get('uid')
-    detaileId = request.GET.get('detaileId')    # 查询详情
-    u_idObjs = models.zgld_userprofile.objects.get(id=uid)
-    company_id = u_idObjs.company_id
+    if request.method == "GET":
 
-    xiaoChengXuObjs = models.zgld_shangcheng_jichushezhi.objects.filter(xiaochengxucompany_id=company_id)
-    indexLunBoTu = ''
-    xiaoChengXuId = ''
-    if xiaoChengXuObjs:
-        indexLunBoTu = xiaoChengXuObjs[0].lunbotu  # 查询首页 轮播图
-        xiaoChengXuId = xiaoChengXuObjs[0].id
-    otherData = []
+        # response = mallManagement.mallManagement(request, uid, goodsGroup, status, flag)
 
-    forms_obj = GoodsManagementSelectForm(request.GET)
-    if forms_obj.is_valid():
-        current_page = forms_obj.cleaned_data['current_page']
-        length = forms_obj.cleaned_data['length']
+        uid = request.GET.get('uid')
+        detaileId = request.GET.get('detaileId')    # 查询详情
+        u_idObjs = models.zgld_userprofile.objects.get(id=uid)
+        company_id = u_idObjs.company_id
 
-        if detaileId:
-            print('=====================xiaoChengXuObjs[0].id.....> ',xiaoChengXuId)
-            objs = models.zgld_goods_management.objects.filter(company_id=company_id).filter(id=detaileId,goodsStatus__in=[1,3])
-            count = objs.count()
-            if objs:
+        xiaoChengXuObjs = models.zgld_shangcheng_jichushezhi.objects.filter(xiaochengxucompany_id=company_id)
+        indexLunBoTu = ''
+        xiaoChengXuId = ''
+        if xiaoChengXuObjs:
+            indexLunBoTu = xiaoChengXuObjs[0].lunbotu  # 查询首页 轮播图
+            xiaoChengXuId = xiaoChengXuObjs[0].id
+        otherData = []
 
-                if length != 0:
-                    start_line = (current_page - 1) * length
-                    stop_line = start_line + length
-                    objs = objs[start_line: stop_line]
+        forms_obj = GoodsManagementSelectForm(request.GET)
+        if forms_obj.is_valid():
+            current_page = forms_obj.cleaned_data['current_page']
+            length = forms_obj.cleaned_data['length']
 
-                print('objs=========>',objs)
-                for obj in objs:
-                    groupObjs = models.zgld_goods_classification_management.objects.filter(id=obj.parentName_id)
-                    xianshangjiaoyi = '否'
-                    if obj.xianshangjiaoyi:
-                        xianshangjiaoyi = '是'
+            if detaileId:
+                print('=====================xiaoChengXuObjs[0].id.....> ',xiaoChengXuId)
+                objs = models.zgld_goods_management.objects.filter(company_id=company_id).filter(id=detaileId,goodsStatus__in=[1,3])
+                count = objs.count()
+                if objs:
 
-                    topLunBoTu = ''
-                    if obj.topLunBoTu:
-                        topLunBoTu = json.loads(obj.topLunBoTu)
-                    detailePicture = ''
-                    if obj.detailePicture:
-                        detailePicture = json.loads(obj.detailePicture)
-                    parentGroup_id = obj.parentName_id
-                    parentGroup_name = obj.parentName.classificationName
-                    if groupObjs[0].parentClassification_id:
-                        parent_group_name = groupObjs[0].parentClassification.classificationName
-                        parentGroup_name = parent_group_name + ' > ' + parentGroup_name
-                    otherData.append({
-                        'id':obj.id,
-                        'goodsName':obj.goodsName,
-                        'parentName_id':parentGroup_id,
-                        'parentName':parentGroup_name,
-                        'goodsPrice':obj.goodsPrice,
-                        'goodsStatus_code':obj.goodsStatus,
-                        'goodsStatus':obj.get_goodsStatus_display(),
-                        'xianshangjiaoyi':xianshangjiaoyi,
-                        'shichangjiage':obj.shichangjiage,
-                        'topLunBoTu': topLunBoTu,                       # 顶部轮播图
-                        'detailePicture' : detailePicture,              # 详情图片
-                        'createDate': obj.createDate.strftime('%Y-%m-%d %H:%M:%S'),
-                        'shelvesCreateDate':obj.shelvesCreateDate.strftime('%Y-%m-%d %H:%M:%S'),
-                        'DetailsDescription': obj.DetailsDescription    # 描述详情
-                    })
+                    if length != 0:
+                        start_line = (current_page - 1) * length
+                        stop_line = start_line + length
+                        objs = objs[start_line: stop_line]
 
-                response.code = 200
-                response.msg = '查询成功'
-                response.data = {
-                     'otherData':otherData,
-                     'count' : count
-                }
-            else:
-                response.code = 302
-                response.msg = '无数据'
+                    print('objs=========>',objs)
+                    for obj in objs:
+                        groupObjs = models.zgld_goods_classification_management.objects.filter(id=obj.parentName_id)
+                        xianshangjiaoyi = '否'
+                        if obj.xianshangjiaoyi:
+                            xianshangjiaoyi = '是'
 
-        else:
-            objs = models.zgld_goods_management.objects.filter(company_id=company_id).exclude(goodsStatus__in=[2,4])
-            count = objs.count()
+                        topLunBoTu = ''
+                        if obj.topLunBoTu:
+                            topLunBoTu = json.loads(obj.topLunBoTu)
+                        detailePicture = ''
+                        if obj.detailePicture:
+                            detailePicture = json.loads(obj.detailePicture)
+                        parentGroup_id = obj.parentName_id
+                        parentGroup_name = obj.parentName.classificationName
+                        if groupObjs[0].parentClassification_id:
+                            parent_group_name = groupObjs[0].parentClassification.classificationName
+                            parentGroup_name = parent_group_name + ' > ' + parentGroup_name
+                        otherData.append({
+                            'id':obj.id,
+                            'goodsName':obj.goodsName,
+                            'parentName_id':parentGroup_id,
+                            'parentName':parentGroup_name,
+                            'goodsPrice':obj.goodsPrice,
+                            'goodsStatus_code':obj.goodsStatus,
+                            'goodsStatus':obj.get_goodsStatus_display(),
+                            'xianshangjiaoyi':xianshangjiaoyi,
+                            'shichangjiage':obj.shichangjiage,
+                            'topLunBoTu': topLunBoTu,                       # 顶部轮播图
+                            'detailePicture' : detailePicture,              # 详情图片
+                            'createDate': obj.createDate.strftime('%Y-%m-%d %H:%M:%S'),
+                            'shelvesCreateDate':obj.shelvesCreateDate.strftime('%Y-%m-%d %H:%M:%S'),
+                            'DetailsDescription': obj.DetailsDescription    # 描述详情
+                        })
 
-            if objs:
-                if length != 0:
-                    start_line = (current_page - 1) * length
-                    stop_line = start_line + length
-                    objs = objs[start_line: stop_line]
-
-
-                for obj in objs:
-                    topLunBoTu = ''
-                    if obj.topLunBoTu:
-                        topLunBoTu = json.loads(obj.topLunBoTu)
-                    otherData.append({
-                        'id':obj.id,
-                        'goodsName': obj.goodsName,
-                        'goodsPrice': obj.goodsPrice,
-                        'topLunBoTu': topLunBoTu,
-                        'shichangjiage': obj.shichangjiage,
-                    })
-
-                if indexLunBoTu:
-                    indexLunBoTu = json.loads(indexLunBoTu)
-
-                response.code = 200
-                response.msg = '查询成功'
-                response.data = {
-                    'indexLunBoTu':indexLunBoTu,
-                    'otherData':otherData,
-                    'count': count
-                }
+                    response.code = 200
+                    response.msg = '查询成功'
+                    response.data = {
+                         'otherData':otherData,
+                         'count' : count
+                    }
+                else:
+                    response.code = 302
+                    response.msg = '无数据'
 
             else:
-                response.code = 302
-                response.msg = '无数据'
+                objs = models.zgld_goods_management.objects.filter(company_id=company_id).exclude(goodsStatus__in=[2,4])
+                count = objs.count()
+
+                if objs:
+                    if length != 0:
+                        start_line = (current_page - 1) * length
+                        stop_line = start_line + length
+                        objs = objs[start_line: stop_line]
+
+
+                    for obj in objs:
+                        topLunBoTu = ''
+                        if obj.topLunBoTu:
+                            topLunBoTu = json.loads(obj.topLunBoTu)
+                        otherData.append({
+                            'id':obj.id,
+                            'goodsName': obj.goodsName,
+                            'goodsPrice': obj.goodsPrice,
+                            'topLunBoTu': topLunBoTu,
+                            'shichangjiage': obj.shichangjiage,
+                        })
+
+                    if indexLunBoTu:
+                        indexLunBoTu = json.loads(indexLunBoTu)
+
+                    response.code = 200
+                    response.msg = '查询成功'
+                    response.data = {
+                        'indexLunBoTu':indexLunBoTu,
+                        'otherData':otherData,
+                        'count': count
+                    }
+
+                else:
+                    response.code = 302
+                    response.msg = '无数据'
+
 
     return JsonResponse(response.__dict__)
 
