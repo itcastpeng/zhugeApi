3877fbae2cbefa658128bf807d8c9c4cb9174652
zhangju <1779038564@qq.com>
2018-12-28 08:55:30 +0800

    订单轮播图

--- zhugeleida/views_dir/xiaochengxu/theOrderManagement.py
+++ zhugeleida/views_dir/xiaochengxu/theOrderManagement.py
@@ -66,9 +66,7 @@ def theOrder(request):
 
                     # [{"url":"statics/zhugeleida/imgs/admin/goods/1545614722212.jpg"}]
                 else:
-                    goods_id = obj.goods_id
-                    goods_objs =  models.zgld_goods_management.objects.filter(id=goods_id)
-                    topLunBoTu = goods_objs[0].topLunBoTu
+                    topLunBoTu = obj.topLunBoTu
 
                 topLunBoTu = json.loads(topLunBoTu)
                 url = topLunBoTu[0].get('data')
