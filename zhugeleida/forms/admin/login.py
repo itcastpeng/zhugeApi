8b91c7520d42e5e8c7b8b9c80674a976c0c1f435
zhangju <1779038564@qq.com>
2018-12-29 11:42:16 +0800

    文章发布提示

--- zhugeleida/forms/admin/login.py
+++ zhugeleida/forms/admin/login.py
@@ -32,10 +32,13 @@ class ModifyPwdForm(forms.Form):   # 重置密码
     def clean_password2(self):
         password1 = self.data.get('password1')
         password2 = self.data.get('password2')
+        if password1 and password2:
+            password1 = password1.strip()
+            password2 = password2.strip()
 
         if password1 == password2:
             if checkPassword(password1):
-                return account.str_encrypt(password2.stip())
+                return account.str_encrypt(password2)
 
             self.add_error('password2', '密码复杂度检查未通过,请检查最小长度为八位,包含大小写字母和数字')
         else:
