8c60b7a9bfac33551cf5ab556e1b9f6c4e27a4e6
zhangju <1779038564@qq.com>
2019-01-07 09:34:32 +0800

    商城财务统计展示

--- zhugeleida/views_dir/xiaochengxu/prepaidManagement.py
+++ zhugeleida/views_dir/xiaochengxu/prepaidManagement.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from zhugeleida.forms.xiaochengxu import yuzhifu_verify
 from zhugeleida.public.common import action_record
-from zhugeleida.views_dir.mycelery_task import  mycelery
+from zhugeleida.views_dir.mycelery_task.mycelery import  record_money_process
 
 
 response = Response.ResponseObj()
@@ -129,7 +129,7 @@ def payback(request):
                         'type': 5     # (5,'商城入账'),
                     }
 
-                    mycelery.record_money_process(record_data)
+                    record_money_process(record_data)
 
 
 
