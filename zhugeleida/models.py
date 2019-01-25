79686f5d2acdb6d6cb9cfd1325aaec060cc7b438
zhangju <1779038564@qq.com>
2019-01-25 09:36:39 +0800

    案例-日记

--- zhugeleida/models.py
+++ zhugeleida/models.py
@@ -1365,29 +1365,29 @@ class zgld_case_tag(models.Model):
 
 #案例库数据表
 class zgld_case(models.Model):
-        user = models.ForeignKey('zgld_admin_userprofile', verbose_name='文章作者', null=True)
-        company = models.ForeignKey('zgld_company', verbose_name='文章所属公司', null=True)
-        customer_name  = models.CharField('zgld_customer', verbose_name='客户姓名', null=True, blank=True)
+    user = models.ForeignKey('zgld_admin_userprofile', verbose_name='文章作者', null=True)
+    company = models.ForeignKey('zgld_company', verbose_name='文章所属公司', null=True)
+    customer_name  = models.CharField(max_length=64,verbose_name='客户姓名', null=True)
 
-        title = models.CharField(verbose_name='文章标题', max_length=128)
-        cover_picture = models.TextField(verbose_name="封面图片URL",null=True)
-        tags = models.ManyToManyField('zgld_article_tag', verbose_name="文章关联的标签")
+    title = models.CharField(verbose_name='文章标题', max_length=128)
+    cover_picture = models.TextField(verbose_name="封面图片URL",null=True)
+    tags = models.ManyToManyField('zgld_article_tag', verbose_name="文章关联的标签")
 
-        read_count = models.IntegerField(verbose_name="阅读数量", default=0)
-        up_count = models.IntegerField(default=0, verbose_name="点赞次数")
-        comment_count = models.IntegerField(default=0, verbose_name="被评论数量")
+    read_count = models.IntegerField(verbose_name="阅读数量", default=0)
+    up_count = models.IntegerField(default=0, verbose_name="点赞次数")
+    comment_count = models.IntegerField(default=0, verbose_name="被评论数量")
 
-        status_choices = ((1, '已发'),
-                          (2, '未发'),
-                          (3, '删除'),  # 逻辑删除
-                          )
-        status = models.SmallIntegerField(default=2, verbose_name='案例状态', choices=status_choices)
-        update_date = models.DateTimeField(verbose_name="日记最后修改时间")
-        create_date = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
+    status_choices = ((1, '已发'),
+                      (2, '未发'),
+                      (3, '删除'),  # 逻辑删除
+                      )
+    status = models.SmallIntegerField(default=2, verbose_name='案例状态', choices=status_choices)
+    update_date = models.DateTimeField(verbose_name="日记最后修改时间")
+    create_date = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
 
-        class Meta:
-            verbose_name_plural = "案例表"
-            app_label = "zhugeleida"
+    class Meta:
+        verbose_name_plural = "案例表"
+        app_label = "zhugeleida"
 
 
 #案例日记表
