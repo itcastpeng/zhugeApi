6809dabaabf0909ca7c3eea12c24a99f8672c5dd
zhangju <1779038564@qq.com>
2019-02-11 16:03:51 +0800

    添加水印

--- zhugeleida/views_dir/public/img_upload.py
+++ zhugeleida/views_dir/public/img_upload.py
@@ -177,9 +177,8 @@ def setup_picture_shuiyin(file_path,company_id,img_source):
 
     d.text((txt.size[0]-150, txt.size[1]-30), shuiyin_name,font=fnt, fill=(255,255,255,255))
     out=Image.alpha_composite(im, txt)
-    # out.show()
+    out.save(file_path)
 
-    return out
 
 
 # 图片上传
