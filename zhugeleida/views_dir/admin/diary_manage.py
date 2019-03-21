1ae470289725744dafd9e41718e76462a9e197b7
zhaoxinpeng <15531506906@sina.cn>
2019-03-21 09:47:19 +0800

    后台 案例 评论操作

--- zhugeleida/views_dir/admin/diary_manage.py
+++ zhugeleida/views_dir/admin/diary_manage.py
@@ -236,7 +236,7 @@ def diary_manage_oper(request, oper_type, o_id):
                 cover_picture = forms_obj.cleaned_data.get('cover_picture')
                 if int(case_type) == 1:  # 普通案例
                     cover_picture = json.loads(cover_picture)
-                    diary_objs.update(cover_picture = cover_picture)
+                    diary_objs.update(cover_picture = json.dumps(cover_picture))
 
                 else:
                     if cover_picture:
