ae7f2d476e913d437e3783e682b03a2696b5deda
zhaoxinpeng <15531506906@sina.cn>
2019-04-01 15:11:47 +0800

    同步文章图片变形问题

--- zhugeleida/views_dir/admin/article.py
+++ zhugeleida/views_dir/admin/article.py
@@ -1712,7 +1712,8 @@ def deal_gzh_picture_url(leixing, url):
     img_tags = soup.find_all('img')
     for img_tag in img_tags:
         if img_tag.attrs.get('style'):
-            img_tag = img_tag.replace(str(img_tag.attrs.get('style')), '')  # 去除行内样式
+            print("str(img_tag.attrs.get('style'))---------> ", str(img_tag.attrs.get('style')))
+            # img_tag = img_tag.replace(, '')  # 去除行内样式
 
         data_src = img_tag.attrs.get('data-src')
         if data_src:
