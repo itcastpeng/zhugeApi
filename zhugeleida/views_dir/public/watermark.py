ba05e5e19ba0f5d44aeebbc02cc8e6d1e754780a
zhaoxinpeng <15531506906@sina.cn>
2019-04-02 15:31:51 +0800

    米扬丽格 水印加重

--- zhugeleida/views_dir/public/watermark.py
+++ zhugeleida/views_dir/public/watermark.py
@@ -34,7 +34,7 @@ class watermark(object):
         im1 = Image.open(imageFile)
         draw = ImageDraw.Draw(im1)               # 绘图句柄
 
-        font = ImageFont.truetype('/usr/share/fonts/chinese/simhei.ttf', 24)  # 使用自定义的字体，第二个参数表示字符大小
+        font = ImageFont.truetype('/usr/share/fonts/chinese/SIMHEI.TTF', 24)  # 使用自定义的字体，第二个参数表示字符大小
         # font = ImageFont.truetype('/usr/share/fonts/chinese/simsun.ttc', 24)  # 使用自定义的字体，第二个参数表示字符大小
         # 文字rgb颜色
         if int(company_id) == 12: # 米扬丽格
