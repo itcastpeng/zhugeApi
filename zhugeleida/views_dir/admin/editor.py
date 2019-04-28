56fcf10cfa4c85f76993a629d2e803dd67e31b7a
zhaoxinpeng <15531506906@sina.cn>
2019-04-28 19:18:42 +0800

    后台权限管理功能

--- zhugeleida/views_dir/admin/editor.py
+++ zhugeleida/views_dir/admin/editor.py
@@ -273,9 +273,9 @@ def editor_oper(request, oper_type, o_id):
                 response.code = 301
                 response.msg = json.loads(forms_obj.errors.as_json())
 
-
-        response.code = 402
-        response.msg = '请求异常'
+        else:
+            response.code = 402
+            response.msg = '请求异常'
 
     return JsonResponse(response.__dict__)
 
