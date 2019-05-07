a523536b7fdfdb4bfa826b224a4ed7f648343bb1
zhaoxinpeng <15531506906@sina.cn>
2019-05-07 15:05:13 +0800

    优化 后台权限管理功能

--- zhugeleida/views_dir/public/img_upload.py
+++ zhugeleida/views_dir/public/img_upload.py
@@ -168,22 +168,17 @@ def img_merge(request):
             img_path = img_path.replace('http://api.zhugeyingxiao.com/', '')
         # 上传到七牛云
         if qiniu and int(qiniu) == 1:
-            print('-0----------------------------------1')
             token = qiniu_oper.qiniu_get_token()
-            print('-0----------------------------------2', token)
-            url = 'https://up-z1.qiniup.com/'
+            qiniu_url = 'https://up-z1.qiniup.com/'
             data = {
                 'token': token,
             }
-            print('-0----------------------------------3')
             files = {
                 'file': open(img_path, 'rb')
             }
-            print('-0----------------------------------4')
-            ret = requests.post(url, data=data, files=files)
-            print('-0----------------------------------5', ret.text)
+            print('qiniu_url------qiniu_url------------qiniu_url-----------qiniu_url---------qiniu_url---------> ', qiniu_url)
+            ret = requests.post(qiniu_url, data=data, files=files)
             os.remove(img_path) #删除本地图片
-            print('-0----------------------------------6')
 
             img_path = 'http://tianyan.zhugeyingxiao.com/' + ret.json().get('key')
         print('-------------返回图片地址-=----------------------------------', img_path)
