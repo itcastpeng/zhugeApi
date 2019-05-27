a759fb59e6b8e38dfbdc6a6456475919d821d127
zhaoxinpeng <15531506906@sina.cn>
2019-05-27 10:54:53 +0800

    雷达首页数据 优化

--- zhugeleida/views_dir/admin/article.py
+++ zhugeleida/views_dir/admin/article.py
@@ -1751,20 +1751,26 @@ def deal_gzh_picture_url(leixing, url):
 
     print('iframe--------> ', iframe)
     for iframe_tag in iframe:
-        shipin_url = iframe_tag.get('data-src')
+        video_tag = iframe_tag.find('video')
         data_cover_url = iframe_tag.get('data-cover')
-        if data_cover_url:
-            data_cover_url = unquote(data_cover_url, 'utf-8')
+        print('video_tag------------------------------> ', video_tag)
+        if video_tag:
+            shipin_url = video_tag.attrs.get('src')
 
-        # print('封面URL data_cover_url ------->>', data_cover_url)
+        else:
+            shipin_url = iframe_tag.get('data-src')
+            if data_cover_url:
+                data_cover_url = unquote(data_cover_url, 'utf-8')
 
-        if '&' in shipin_url and 'vid=' in shipin_url:
-            vid_num = shipin_url.split('vid=')[1]
-            _url = shipin_url.split('?')[0]
-            shipin_url = _url + '?vid=' + vid_num
-            # data-src="https://v.qq.com/iframe/preview.html?width=500&amp;height=375&amp;auto=0&amp;vid=z08202kwzjw"
+            # print('封面URL data_cover_url ------->>', data_cover_url)
 
-        # print('视频链接 shipin_url----->>\n', shipin_url)
+            if '&' in shipin_url and 'vid=' in shipin_url:
+                vid_num = shipin_url.split('vid=')[1]
+                _url = shipin_url.split('?')[0]
+                shipin_url = _url + '?vid=' + vid_num
+                # data-src="https://v.qq.com/iframe/preview.html?width=500&amp;height=375&amp;auto=0&amp;vid=z08202kwzjw"
+
+            # print('视频链接 shipin_url----->>\n', shipin_url)
         iframe_tag.attrs['data-src'] = shipin_url
         iframe_tag.attrs['allowfullscreen'] = True
         iframe_tag.attrs['data-cover'] = data_cover_url  # 'http://statics.api.zhugeyingxiao.com/' + data_cover_url
