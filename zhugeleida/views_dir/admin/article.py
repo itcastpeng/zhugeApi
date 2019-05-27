d3cec57b9f128b0c38058a469ce598124f483b4c
zhaoxinpeng <15531506906@sina.cn>
2019-05-27 11:52:19 +0800

    雷达首页数据 优化

--- zhugeleida/views_dir/admin/article.py
+++ zhugeleida/views_dir/admin/article.py
@@ -1750,23 +1750,23 @@ def deal_gzh_picture_url(leixing, url):
     for iframe_tag in iframe:
         print("iframe_tag.find('div', id='page-content')-----------------------> ", iframe_tag.find('html'))
         print("iframe_tag.find('div', id='page-content')-----------------------> ", iframe_tag.attrs.get('document'))
-        data_cover_url = iframe_tag.get('data-cover')
-        shipin_url = iframe_tag.get('data-src')
-        if data_cover_url:
-            data_cover_url = unquote(data_cover_url, 'utf-8')
-        if shipin_url not in [
-            'https://mp.weixin.qq.com/mp/readtemplate?t=pages/video_player_tmpl&action=mpvideo&auto=0&vid=wxv_800489262956412928',
-        ]:
-
-            if '&' in shipin_url and 'vid=' in shipin_url:
-                vid_num = shipin_url.split('vid=')[1]
-                _url = shipin_url.split('?')[0]
-                shipin_url = _url + '?vid=' + vid_num
-
-
-        iframe_tag.attrs['data-src'] = shipin_url
-        iframe_tag.attrs['allowfullscreen'] = True
-        iframe_tag.attrs['data-cover'] = data_cover_url  # 'http://statics.api.zhugeyingxiao.com/' + data_cover_url
+        # data_cover_url = iframe_tag.get('data-cover')
+        # shipin_url = iframe_tag.get('data-src')
+        # if data_cover_url:
+        #     data_cover_url = unquote(data_cover_url, 'utf-8')
+        # if shipin_url not in [
+        #     'https://mp.weixin.qq.com/mp/readtemplate?t=pages/video_player_tmpl&action=mpvideo&auto=0&vid=wxv_800489262956412928',
+        # ]:
+        #
+        #     if '&' in shipin_url and 'vid=' in shipin_url:
+        #         vid_num = shipin_url.split('vid=')[1]
+        #         _url = shipin_url.split('?')[0]
+        #         shipin_url = _url + '?vid=' + vid_num
+        #
+        #
+        # iframe_tag.attrs['data-src'] = shipin_url
+        # iframe_tag.attrs['allowfullscreen'] = True
+        # iframe_tag.attrs['data-cover'] = data_cover_url  # 'http://statics.api.zhugeyingxiao.com/' + data_cover_url
 
     content = str(style) + str(body)
     # print('最后的html---->>', content)
