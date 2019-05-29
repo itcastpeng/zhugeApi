ae39cdccf25e49e90e9b464c66cd4c41cfd97f0c
zhaoxinpeng <15531506906@sina.cn>
2019-05-29 11:12:59 +0800

    雷达优化 同步文章

--- zhugeleida/views_dir/mycelery_task/mycelery_expand.py
+++ zhugeleida/views_dir/mycelery_task/mycelery_expand.py
@@ -302,11 +302,11 @@ def batchget_article_material(request):
                         item_list = _response.data.get('item')  # 获取的素材文章列表
                         for item in item_list: # 大列表
                             media_id = item.get('content').get('news_item')
-                            update_time = int( item.get('update_time'))
+                            update_time = item.get('update_time')
                             content_list = item.get('content').get('news_item') # 嵌套的小列表
-                            print('update_time---------------> ', update_time)
+                            print('update_time---------------> ', update_time, type(update_time))
                             for content in content_list:
-                                ltime = time.localtime(update_time)
+                                ltime = time.localtime(int(update_time))
                                 update_time = time.strftime('%Y-%m-%d %H:%M:%S', ltime)
 
                                 title = content.get('title')
