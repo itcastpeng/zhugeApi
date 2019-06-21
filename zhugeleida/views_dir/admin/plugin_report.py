ac9a9892dddfb2fee3be62d4709d1b1807d3b0a2
zhaoxinpeng <15531506906@sina.cn>
2019-06-21 13:47:31 +0800

    雷达AI 数据统计 优化

--- zhugeleida/views_dir/admin/plugin_report.py
+++ zhugeleida/views_dir/admin/plugin_report.py
@@ -56,11 +56,8 @@ def plugin_report(request, oper_type):
 
                 # 获取第几页的数据
                 for obj in objs:
-                    print('vobj.zgld_article_set------------> ', obj.zgld_article_set)
-                    print('vobj.zgld_article_set------------> ', obj.zgld_article_set.all())
-                    print('vobj.zgld_article_set------------> ', obj.zgld_article_set.filter(plugin_report_id=obj.id))
                     is_update_plugin = True  # 是否可以修改报名插件
-                    if obj.zgld_article_set.filter(plugin_report_id=obj.id).exclude(status=3): # 如果有文章使用了报名插件 则不能修改
+                    if models.zgld_article.objects.filter(status__in=[1, 2], plugin_report_id=obj.id): # 如果有文章使用了报名插件 则不能修改
                         is_update_plugin = False
 
                     read_count = obj.read_count
